f505beb6ba46f6a9e3a7eb7f66a9a4e2fffee883
ZoloLua <zolo79pa@gmail.com>
2025-09-05 18:31:44 -0700

    Update OmegaLoader.lua

--- OmegaLoader.lua
+++ OmegaLoader.lua
@@ -1,1 +1,7 @@
+--[[
+Obfuscatored by Zolo Obfuscator
+discord: unknown
+2025 Zolo Obfuscator®
+]]
+
 loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/2c936f061e1dc4ea1c8a95339a6054b1"))()
